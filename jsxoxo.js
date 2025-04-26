6b067575d0e6a2a9461463c8951593f5fcfe4f60
Gabriela <gaby.ro23@yahoo.com>
2025-04-26 23:04:14 +0200

    Outcome message

--- jsxoxo.js
+++ jsxoxo.js
@@ -3,7 +3,18 @@ const sectionGame = document.getElementById('game')
 const firstStep = document.getElementById('first-step')
 const restartButton = document.getElementById('restart')
 const sectionRestart = document.getElementById('end')
-let gameButtons = document.getElementsByClassName('box')
+const boxes = document.querySelectorAll('.box')
+const box1 = document.getElementById('box1')
+const box2 = document.getElementById('box2')
+const box3 = document.getElementById('box3')
+const box4 = document.getElementById('box4')
+const box5 = document.getElementById('box5')
+const box6 = document.getElementById('box6')
+const box7 = document.getElementById('box7')
+const box8 = document.getElementById('box8')
+const box9 = document.getElementById('box9')
+const spanOutcome = document.getElementById('outcome')
+let outcome = ""
 
 let computerSelection
 let playerSelection
@@ -45,25 +56,34 @@ function selectionO(){
 function match(){
     firstStep.style.display = 'none'
     sectionGame.style.display = 'grid'
-    gameButtons.forEach( box => {
-        box.addEventListener('click', handleClick)
-    })
+    boxes.forEach(box => {
+        box.addEventListener('click', showSelection)
+    });
 }
 
-function handleClick(event){
-    const handledBox = event.target;
-    alert("Lets see")
+function showSelection(event){
+    const box = event.target
+    if (box.textContent !== '') return;
+    box.textContent = playerSelection
+    competition()
 }
 
-    const box1 = document.getElementById('box1')
-        box1.addEventListener('click', "X")
-    const box2 = document.getElementById('box2')
-    const box3 = document.getElementById('box3')
-    const box4 = document.getElementById('box4')
-    const box5 = document.getElementById('box5')
-    const box6 = document.getElementById('box6')
-    const box7 = document.getElementById('box7')
-    const box8 = document.getElementById('box8')
-    const box9 = document.getElementById('box9')
+function competition(){
+    if (box1.textContent !== '' &&
+        box2.textContent !== '' &&
+        box3.textContent !== '' &&
+        box1.textContent === box2.textContent &&
+        box2.textContent === box3.textContent){
+        outcome = 'You won bitch'
+        }
+    if (outcome !== ''){
+        alert(outcome);
+        showOutcomeAndRestart()
+    }
+}
 
+function showOutcomeAndRestart(){
+    sectionRestart.style.display = 'inline'
+    spanOutcome.innerHTML = outcome
+}
 
